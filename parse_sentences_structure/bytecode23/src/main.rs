daae5119624e9a1f9dcef3892a222adc2ae722fe
noname <noname@noname>
2025-03-24 09:30:15 +0900

    Add bytecode23 for understand definition of bytecode.

---
+++ parse_sentences_structure/bytecode23/src/main.rs
@@ -0,0 +1,33 @@
+enum Instruction {
+    LoadLiteral(i64),
+    Add,
+  }
+  
+  fn main() {
+    let instructions = [
+      Instruction::LoadLiteral(42),
+      Instruction::LoadLiteral(36),
+      Instruction::Add,
+    ];
+  
+    let result = interpret(&instructions);
+  
+    println!("result: {result:?}");
+  }
+  
+  fn interpret(instructions: &[Instruction]) -> Option<i64> {
+    let mut stack = vec![];
+  
+    for instruction in instructions {
+      match instruction {
+        Instruction::LoadLiteral(value) => stack.push(*value),
+        Instruction::Add => {
+          let rhs = stack.pop().expect("Stack underflow");
+          let lhs = stack.pop().expect("Stack underflow");
+          stack.push(lhs + rhs);
+        }
+      }
+    }
+  
+    stack.pop()
+  }
